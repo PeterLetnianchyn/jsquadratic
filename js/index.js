d8c764d1408684a6ac184c4e4b83c50e2d0fe2e3
Peter <katalol95@gmail.com>
2018-11-21 08:54:39 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const a = prompt('Enter a');
+const b = prompt('Enter b');
+const c = prompt('Enter c');
+let result = quadraticEquation(a, b, c);
+alert('Answer is' + result);
+    
+function quadraticEquation(a, b, c) {
+	if (a == 0) return "Error, а=0 Invalid value!";
+	let D = b * b - 4 * a * c; //discriminator
+	let x1, x2;
+	if (D > 0) {
+		x1 = (-b + Math.sqrt(D)) / (2 * a);
+		x2 = (-b - Math.sqrt(D)) / (2 * a);
+		return "coefficients: a="+a+", b="+b+", c="+c+"the roots of the equation: X1=" + x1 + ", X2=" + x2 + "discriminator(D)=" + D;
+	} else if (D == 0) {
+		x1 = -b / (2 * a);
+		return "coefficients: a="+a+", b="+b+", c="+c+"the roots of the equation: X=" + x1 + "discriminator(D)=" + D;
+	} else if (D < 0) return "coefficients: a="+a+", b="+b+", c="+c+"no root!discriminator(D)=" + D;
+
+}
+
